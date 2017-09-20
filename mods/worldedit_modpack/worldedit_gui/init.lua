c279ed254a527c24f1470542ec443c2f85351a5a
JPG <jeanpatrick.guerrero@gmail.com>
2017-09-20 11:59:25 +0200

    update worldedit

--- mods/worldedit_modpack/worldedit_gui/init.lua
+++ mods/worldedit_modpack/worldedit_gui/init.lua
@@ -58,7 +58,7 @@ end
 
 worldedit.get_formspec_header = function(identifier)
 	local entry = worldedit.pages[identifier] or {}
-	return "button[0,0;2,0.5;worldedit_gui;Back]" ..
+	return "button[0,0;2,0.5;worldedit_gui;< Back]" ..
 		string.format("label[2,0;WorldEdit GUI > %s]", entry.name or "")
 end
 
@@ -70,71 +70,7 @@ local get_formspec = function(name, identifier)
 end
 
 --implement worldedit.show_page(name, page) in different ways depending on the available APIs
-if rawget(_G, "unified_inventory") then --unified inventory installed
-	local old_func = worldedit.register_gui_function
-	worldedit.register_gui_function = function(identifier, options)
-		old_func(identifier, options)
-		unified_inventory.register_page(identifier, {get_formspec=function(player) return {formspec=options.get_formspec(player:get_player_name())} end})
-	end
-
-	unified_inventory.register_button("worldedit_gui", {
-		type = "image",
-		image = "inventory_plus_worldedit_gui.png",
-	})
-
-	minetest.register_on_player_receive_fields(function(player, formname, fields)
-		local name = player:get_player_name()
-		if fields.worldedit_gui then --main page
-			worldedit.show_page(name, "worldedit_gui")
-			return true
-		elseif fields.worldedit_gui_exit then --return to original page
-			local player = minetest.get_player_by_name(name)
-			if player then
-				unified_inventory.set_inventory_formspec(player, "craft")
-			end
-			return true
-		end
-		return false
-	end)
-
-	worldedit.show_page = function(name, page)
-		local player = minetest.get_player_by_name(name)
-		if player then
-			player:set_inventory_formspec(get_formspec(name, page))
-		end
-	end
-elseif rawget(_G, "inventory_plus") then --inventory++ installed
-	minetest.register_on_joinplayer(function(player)
-		local can_worldedit = minetest.check_player_privs(player:get_player_name(), {worldedit=true})
-		if can_worldedit then
-			inventory_plus.register_button(player, "worldedit_gui", "WorldEdit")
-		end
-	end)
-
-	--show the form when the button is pressed and hide it when done
-	local gui_player_formspecs = {}
-	minetest.register_on_player_receive_fields(function(player, formname, fields)
-		local name = player:get_player_name()
-		if fields.worldedit_gui then --main page
-			gui_player_formspecs[name] = player:get_inventory_formspec()
-			worldedit.show_page(name, "worldedit_gui")
-			return true
-		elseif fields.worldedit_gui_exit then --return to original page
-			if gui_player_formspecs[name] then
-				inventory_plus.set_inventory_formspec(player, inventory_plus.get_formspec(player, "main"))
-			end
-			return true
-		end
-		return false
-	end)
-
-	worldedit.show_page = function(name, page)
-		local player = minetest.get_player_by_name(name)
-		if player then
-			inventory_plus.set_inventory_formspec(player, get_formspec(name, page))
-		end
-	end
-elseif rawget(_G, "sfinv") and minetest.get_modpath("teacher_menu") then
+if rawget(_G, "sfinv") and minetest.get_modpath("teacher_menu") then
 	assert(sfinv.enabled)
 	local orig_get = sfinv.pages["teacher_menu"].get
 	sfinv.override_page("teacher_menu", {
@@ -270,7 +206,7 @@ worldedit.register_gui_function("worldedit_gui", {
 			y = y - height
 		end
 		return string.format("size[%g,%g]", math.max(columns * width, 5), math.max(y + 0.5, 3)) ..
-			"button[0,0;2,0.5;worldedit_gui_exit;Back]" ..
+			"button[0,0;2,0.5;worldedit_gui_exit;< Back]" ..
 			"label[2,0;WorldEdit GUI]" ..
 			table.concat(buttons)
 	end,
