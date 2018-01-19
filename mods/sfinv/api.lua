d99566058230fabfd32200f9c1e53c87153a2e99
JPG <jeanpatrick.guerrero@gmail.com>
2018-01-19 15:27:30 +0100

    update

--- mods/sfinv/api.lua
+++ mods/sfinv/api.lua
@@ -91,7 +91,7 @@ function sfinv.get_formspec(player, context)
 		if not sfinv.pages[context.page] then
 			context.page = "creative:all"
 		end
-		assert(sfinv.pages[context.page], "[sfinv] Invalid homepage")
+		--assert(sfinv.pages[context.page], "[sfinv] Invalid homepage")
 		minetest.log("warning", "[sfinv] Couldn't find " .. dump(old_page) .. " so using switching to homepage")
 		return sfinv.get_formspec(player, context)
 	end
